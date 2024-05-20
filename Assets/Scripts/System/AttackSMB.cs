1bf7d10cac3ee45f8dea59f40a6140cd198b7e0e
Loomip <149350721+Loomip@users.noreply.github.com>
2024-05-20 16:16:31 +0900

    20240520 End

--- Assets/Scripts/System/AttackSMB.cs
+++ Assets/Scripts/System/AttackSMB.cs
@@ -4,13 +4,11 @@ public class AttackSMB : StateMachineBehaviour
 {
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        var attacking = animator.GetComponent<AttackController>().IsAttack = false;
-        Debug.Log($"움직이면 안됨{attacking}");
+        animator.GetComponent<AttackController>().IsAttack = false;
     }
 
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        var attacking = animator.GetComponent<AttackController>().IsAttack = true;
-        Debug.Log($"움직여야됨{attacking}");
+        animator.GetComponent<AttackController>().IsAttack = true;
     }
 }
